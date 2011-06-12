7dbe49864b0d13457a67e29be27b30dab6730fce
Brandyn A. White <bwhite@dappervision.com>
2011-06-12 16:15:23 -0400

    Added frame skip

--- keyframe/histogram.py
+++ keyframe/histogram.py
@@ -5,12 +5,13 @@ import numpy as np
 
 class Histogram(object):
 
-    def __init__(self, diff_thresh=10):
+    def __init__(self, diff_thresh=10, skip_mod=1):
         self._diff_thresh = diff_thresh
+        self._skip_mod = skip_mod
 
     def __call__(self, video):
         prev_vec = None
-        for frame_num, frame_time, frame in vidfeat.convert_video(video, ('frameiter', [('opencv', 'bgr', 8)])):
+        for frame_num, frame_time, frame in vidfeat.convert_video(video, ('frameiterskip', [('opencv', 'bgr', 8)], self._skip_mod)):
             width, height = frame.width, frame.height
             cgr = imfeat.CoordGeneratorRect
             out = []
